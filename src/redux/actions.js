975209ddfe1b2980945be742e8a65c879acf6dd3
Semih Ozdogan <semih.ozdogan@ecozum.com>
2022-09-21 14:19:50 +0300

    filter added

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-export const getCoins = skip => {
+export const getCoins = (skip, callback) => {
     return (dispatch) => {
         const URL = "https://api.coinstats.app/public/v1/coins?limit=20&skip=" + skip;
         fetch(URL)
@@ -6,6 +6,7 @@ export const getCoins = skip => {
             .then(responseJson => {
                 dispatch(
                     { type: "getCoins", payload: responseJson.coins })
+                callback(responseJson.coins);
             })
             .catch(err =>
                 console.log(err))
--- src/screens/Coins.js
+++ src/screens/Coins.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { FlatList, StyleSheet, View, SafeAreaView, ActivityIndicator, TextInput, TouchableOpacity } from "react-native";
+import { FlatList, StyleSheet, View, SafeAreaView, ActivityIndicator, TextInput, TouchableOpacity, Text } from "react-native";
 import ListItem from "../../src/components/ListItem"
 import Icon from "react-native-vector-icons/FontAwesome";
 import SplashScreen from "react-native-splash-screen";
 import { connect } from "react-redux";
 import { getCoins } from "../redux/actions";
 
-function Coins(props) {
+function Coins({ getCoins, navigation }) {
 
     const
         [skip, setSkip] = useState(0),
         [loading, setLoading] = useState(false),
         [loadPage, setLoadPage] = useState(true),
+        [data, setData] = useState([]),
+        [newData, setNewData] = useState([]),
+        [dataNotFound, setDataNotFound] = useState(true),
         [text, setText] = useState(""),
-        { container, indicatorContainer, footerContainer, headerContainer, headerSubContainer, headerIconContainer, hedaerBtnContainer, textInput, icon } = styles,
+        { container, indicatorContainer, footerContainer, headerContainer, headerSubContainer, headerIconContainer, hedaerBtnContainer, textInput, icon, notFound, txt } = styles,
 
         loadMore = () => {
             setSkip(skip + 1)
@@ -23,33 +26,55 @@ function Coins(props) {
             if (loading) return null;
             return (
                 <View style={footerContainer}>
-                    <ActivityIndicator animating size="large" color="white" />
+                    <ActivityIndicator animating size="large" color="red" />
                 </View>
             );
         },
 
         searchFilterFunction = (text) => {
-            setText(text)
-            console.log(text);
+            if (text.length >= 3) {
+                const newData = data.filter(item => {
+                    const itemData = item.name
+                        ? item.name.toUpperCase()
+                        : ''.toUpperCase();
+                    const textData = text.toUpperCase();
+                    return itemData.indexOf(textData) > -1;
+                });
+                setDataNotFound(newData.length === 0 ? false : true);
+                setNewData(newData);
+                setText(text);
+                setLoading(true)
+            }
+            else {
+                setDataNotFound(true)
+                setLoading(false)
+                setNewData(data);
+                setText(text);
+            }
         },
 
         sortList = () => {
-            console.log("sorting");
+            const sortedData = [...newData].sort((a, b) => {
+                return a.name > b.name;
+            })
+            setNewData(sortedData)
         };
-
     useEffect(() => {
         SplashScreen.hide();
         setTimeout(() => {
             setLoadPage(false)
         }, 1000);
-        props.getCoins(skip)
+        getCoins(skip, response => {
+            setData([...data, ...response]);
+            setNewData([...newData, ...response]);
+        })
     }, [skip])
 
     return (
         !loadPage ?
             <SafeAreaView style={container}>
                 <FlatList
-                    data={props.data}
+                    data={newData}
                     keyExtractor={(item, index) => index}
                     onEndReached={loadMore}
                     ListFooterComponent={renderFooter}
@@ -65,7 +90,7 @@ function Coins(props) {
                                 />
                             </View>
                             <View style={headerIconContainer}>
-                                <TouchableOpacity onPress={sortList} style={hedaerBtnContainer}>
+                                <TouchableOpacity onPress={() => sortList()} style={hedaerBtnContainer}>
                                     <Icon name="sort" size={25} style={icon} />
                                 </TouchableOpacity>
                             </View>
@@ -78,12 +103,18 @@ function Coins(props) {
                             price={item.price}
                             priceBtc={item.priceBtc}
                             icon={item.icon}
-                            onPress={() => props.navigation.navigate("Detail", {
+                            onPress={() => navigation.navigate("Detail", {
                                 data: item
                             })}
                         />
+
                     }
                 />
+                {!dataNotFound && (
+                    <View style={notFound}>
+                        <Text style={txt}>Kullanıcı Bulunamadı</Text>
+                    </View>
+                )}
             </SafeAreaView >
             : <SafeAreaView style={indicatorContainer}><ActivityIndicator animating size="large" color="white" /></SafeAreaView>
 
@@ -93,7 +124,7 @@ function Coins(props) {
 const mapStateToProps = (state) => ({
     data: state.data,
 }),
-    mapDispatchToProps = { getCoins },
+    mapDispatchToProps = { getCoins: (skip, callback) => getCoins(skip, callback) },
 
     styles = StyleSheet.create({
         container: { flex: 1, backgroundColor: "#2C3E50" },
@@ -107,7 +138,9 @@ const mapStateToProps = (state) => ({
         headerIconContainer: { flex: 1, alignItems: "center", justifyContent: "center" },
         hedaerBtnContainer: { backgroundColor: "#021B2C", width: 50, padding: 10, borderRadius: 4, alignItems: "center" },
         textInput: { color: "white", justifyContent: "center", alignItems: "center", flex: 1, marginLeft: 10 },
-        icon: { color: "#ccc" }
+        icon: { color: "#ccc" },
+        notFound: { width: "100%", height: "100%", justifyContent: "center", alignItems: "center", position: "absolute" },
+        txt: { color: "white" }
     });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Coins);
